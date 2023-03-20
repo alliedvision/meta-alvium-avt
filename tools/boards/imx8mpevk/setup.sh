b9e39583ee864f78d7eba059c9a445a003e82cbd
Dennis Langenkamp <dennis.langenkamp@alliedvision.com>
2023-03-20 11:49:09 +0100

    Added first version of new setup script with configuration for the imx8mpevk

---
+++ tools/boards/imx8mpevk/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+IMX_BSP_DIR=${BSP_DIR}/imx-bsp
+
+echo "Setup for i.MX8M Plus EVK"
+
+echo "Build dir ${BUILD_DIR}"
+
+cd ${IMX_BSP_DIR}
+
+MACHINE="imx8mpevk"
+DISTRO="fsl-imx-xwayland"
+
+IMX_BUILD_DIR=$(realpath --relative-to=${PWD} ${BUILD_DIR})
+
+if [ ${FULL_SETUP} -eq 1 ]; then
+    source imx-setup-release.sh -b ${IMX_BUILD_DIR}
+
+    BUILD_DIR=.
+
+    if [ ! -f conf/bblayers.conf.imx ]; then
+        cp conf/bblayers.conf conf/bblayers.conf.imx
+    fi
+
+    sed -i 's/${BSPDIR}\/sources/${BSPDIR}\/imx-bsp\/sources/g' conf/bblayers.conf
+
+    if [ -d "${BSP_DIR}/sources/meta-imx-avt" ]; then
+
+        if [ -z $(grep "meta-imx-avt" ${BBLAYERS_FILE}) ]; then
+            echo "BBLAYERS += \"\${BSPDIR}/sources/meta-imx-avt\"" >> ${BBLAYERS_FILE}
+        fi
+    else
+        echo "Warning: meta-imx-avt not found! Some feature may not be available!"
+    fi
+else
+    source setup-environment ${IMX_BUILD_DIR}
+fi
